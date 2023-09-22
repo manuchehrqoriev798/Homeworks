6f0f58c305de87dae716d4a6c1640d3c2ee44dff
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-09-22 14:46:08 +0600

    test

--- test.py
+++ test.py
@@ -1,2 +1,29 @@
-test = true
-print(test)
+import cv2
+
+# Open a connection to the camera (0 represents the default camera, change it if you have multiple cameras)
+cap = cv2.VideoCapture(1)
+
+# Check if the camera opened successfully
+if not cap.isOpened():
+    print("Error: Could not open camera.")
+    exit()
+
+while True:
+    # Capture a frame from the camera
+    ret, frame = cap.read()
+
+    # Check if the frame was captured successfully
+    if not ret:
+        print("Error: Could not read frame.")
+        break
+
+    # Display the frame
+    cv2.imshow('Camera', frame)
+
+    # Break the loop when the 'q' key is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the camera and close the OpenCV window
+cap.release()
+cv2.destroyAllWindows()
