6ae107b3e5d07b7c8a4d8d5338ef99babd79e2cc
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-09-24 14:11:12 +0600

    test

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import cv2
 
 # Open a connection to the camera (0 represents the default camera, change it if you have multiple cameras)
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 
 # Check if the camera opened successfully
 if not cap.isOpened():
